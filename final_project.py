21813f91511f5404160264bc8b6cf587b66dbfde
ashmwu <ashmwu@github.com>
2020-11-10 22:42:27 -0500

    Create final_project.py

---
+++ final_project.py
@@ -0,0 +1,26 @@
+#find a database with a list of songs to import from then replace the line below
+import playlist from database
+class MusicApp:
+    """ A music selection app similar to Spotify. 
+        Creates/plays playlist based on user's mood.
+        Create artist profiles that includes their music sorted by album release dates which users can listen to.
+        
+        
+        Attributes:
+            
+    """
+    def __init__():
+        """ initialize variables that we need
+            Will change as we work on code
+        """
+        mood = [happy, sad, angry, frusterated, jealously, heartbreak, excited]
+        artist = [Ariana Grande, BTS, Justin Bieber, Pop Smoke]
+        genre = [EDM, Pop, Rap, Lo-fi, K-Pop, Country, Indie]
+    def mood():
+        """Select a mood to be able to get a list of songs that match the mood. """
+    def shuffle():
+        """ Randomly shuffles the music"""
+    def function():
+        """ """
+    def function2():
+        """ """
