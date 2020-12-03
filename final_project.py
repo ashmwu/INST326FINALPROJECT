03eb51c6e8181aba6b23dbac87bd80e9424117a2
Mawi23 <74553193+Mawi23@users.noreply.github.com>
2020-12-03 18:59:58 -0500

    added some stuff to the suggest function

--- final_project.py
+++ final_project.py
@@ -1,5 +1,9 @@
 #find a database with a list of songs to import from then replace the line below
 import playlist from database
+
+import csv 
+
+
 class MusicApp:
     """ A music selection app similar to Spotify. 
         Creates/plays playlist based on user's mood.
@@ -13,6 +17,7 @@ class MusicApp:
         """ initialize variables that we need
             Will change as we work on code
         """
+        
         mood = [happy, sad, angry, frusterated, jealously, heartbreak, excited]
         artist = [Ariana Grande, BTS, Justin Bieber, Pop Smoke]
         genre = [EDM, Pop, Rap, Lo-fi, K-Pop, Country, Indie]
@@ -24,16 +29,25 @@ class MusicApp:
         """ """
     def function2():
         """ """
-    def suggest():
-        """ Based off recent history make music suggestions. Make 
-        suggestions based on artist, genre, album """
+    def suggest(self, genre, artist):
+        """ The user enters an artist's name and the function returns that artist's more popular songs.  """
+        
+        with open('Musiclist_326_projects.csv') as f:
+            spreadsheet = csv.Dictreader(f)
+            for row in spreadsheet:
+                if row['popularity'] > 50:
+                    print(row['genre'],row['artist_name'])
+                
+                    
+        
+        
+        
     def share():
         """Share music currently listening to on social media """
     def library():
         """ This will store all music that has been downloaded to the library"""
     def lyrics():
         """Will pull up the lyrics of a song"""
-    def recently_played():
-        """Creates a list of the ten most recently played songs."""
+    def 
     
         
