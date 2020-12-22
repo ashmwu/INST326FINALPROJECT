d9dfa78f9e682c1e1b7104194fab1d42277e6948
skim225 <70543515+skim225@users.noreply.github.com>
2020-12-22 17:34:01 -0500

    small changes

--- test_final_project.py
+++ test_final_project.py
@@ -1,8 +1,15 @@
+import pytest
 import final_project as fp
 
 def test_recently_searched():
-    x = ['']
-    assert fp.recently_searched(x) == 
+    x = ['T-pain', 'SZA', 'Dreezy', 'John Legend', 'Labrinth', 'Twista', 'dvsn', 
+         'Weezer', 'Flyleaf', 'Frank Ocean', 'Joji']
+    assert fp.recently_searched(x) == ['Joji', 'Frank Ocean', 'Flyleaf', 'Weezer', 'dvsn',
+                                       'Twista', 'Labrinth', 'John Legend', 'Dreezy', 'SZA']
     
 def test_createNewUser():
-    assert fp.createNewUser() == 
+    m = fp.MusicApp('bob')
+    #with mock
+    user = m.createNewUser('bob')
+    assert user.username == 'bob'
+    assert user.fav_genres == []
