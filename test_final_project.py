f1bceab207c2ad6468c9dffc4293f97f1110abf8
ashmwu <ashmwu@github.com>
2020-12-22 23:54:47 -0500

    FINAL TEST

--- test_final_project.py
+++ test_final_project.py
@@ -17,3 +17,22 @@ def test_recently_searched():
          'Weezer', 'Flyleaf', 'Frank Ocean', 'Joji']
     assert recently_searched(x) == ['Joji', 'Frank Ocean', 'Flyleaf', 'Weezer', 'dvsn',
                                        'Twista', 'Labrinth', 'John Legend', 'Dreezy', 'SZA']
+    
+def test_search():
+    
+    #Assert statements for when the user chooses a specific artist name 
+    
+    artist = 'Frank Ocean'
+    assert search(artist) == 7, 'R&B', 'Frank Ocean', 'Seigfried'
+    assert search(artist) == 141, 'R&B', 'Frank Ocean', 'Bad Religion'
+    assert search(artist) == 595, 'Alternative', 'Frank Ocean', 'Nights'
+    assert search(artist) == 7, 'Alternative', 'Frank Ocean', 'Thinkin Bout You'
+    assert search(artist) == 7, 'Alternative', 'Frank Ocean', 'Ivy'
+    
+    #Assert statements for when the user chooses a specific song 
+    
+    track = 'Get You Good'
+    assert search(track) ==  21, 'R&B', 'Roy Woods', 'Get You Good'
+    assert search(track) ==  45451, 'Hip-Hop', 'Roy Woods', 'Get You Good'
+    assert search(track) ==  66345, 'pop', 'Roy Woods', 'Get You Good'
+    assert search(track) ==  70207, 'Rap', 'Roy Woods', 'Get You Good'
